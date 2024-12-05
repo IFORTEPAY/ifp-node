6eeef6fd7b297081f88cc19ed9ae99da69ca3666
arib_billah <arib.billah@mcpayment.co.id>
2024-12-05 11:58:56 +0700

    fixing error lint

--- util/response.ts
+++ util/response.ts
@@ -1,6 +1,6 @@
 import { ApiResponse } from "./type";
 
-export function sendInternalServerError<T>(err: any): ApiResponse<T> {
+export function sendInternalServerError<T>(err: Error): ApiResponse<T> {
   return {
     response_code: "98",
     response_message: "internal server error",
