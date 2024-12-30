33ff79698dea05974c52aabfbcc0734eb775c0e2
arib_billah <arib.billah@mcpayment.co.id>
2024-12-30 10:21:01 +0700

    CARD-1007 add unit test for charge direct

--- test/pg/constant/card.ts
+++ test/pg/constant/card.ts
@@ -1,4 +1,8 @@
-import {RequestCharge} from "../../../pg/card/utils/type";
+import {
+	RequestCharge,
+	RequestChargeDirect,
+	ResponseDataChargeDirectJSON,
+} from "../../../pg/card/utils/type";
 
 export const REQUEST_CARD_CHARGE: RequestCharge = {
 	externalId: "externalId0000001",
@@ -27,4 +31,34 @@ export const REQUEST_CARD_CHARGE: RequestCharge = {
 export const RESPONSE_CARD_CHARGE = {
 	HTML: "<div><p>dummy html result</p></div>",
 	LINK: "https://dummy-link-result.com",
+};
+
+export const REQUEST_CARD_CHARGE_DIRECT_V1: RequestChargeDirect = {
+	...REQUEST_CARD_CHARGE,
+};
+
+export const REQUEST_CARD_CHARGE_DIRECT_V2: RequestChargeDirect = {
+	...REQUEST_CARD_CHARGE,
+	paymentChannel: "BCACC",
+};
+
+export const RESPONSE_CARD_CHARGE_DIRECT_V1: ResponseDataChargeDirectJSON = {
+	token: "b0e3db06-1bde-49f8-bba6-471bc7cd9d18",
+	status: "SUCCESS",
+	transaction_code: "00",
+	transaction_description:
+		"Approved: The transaction charge direct v1 was successful",
+	transaction_data: {
+		external_id: "externalId0000001",
+		receipt_no: "412710059461",
+		order_id: "orderId000000001",
+		transaction_id: "4eab1441-8308-4fa0-91b8-969973e0b0fd",
+		approval_code: "059461",
+	},
+};
+
+export const RESPONSE_CARD_CHARGE_DIRECT_V2: ResponseDataChargeDirectJSON = {
+	...RESPONSE_CARD_CHARGE_DIRECT_V1,
+	transaction_description:
+		"Approved: The transaction charge direct v2 was successful",
 };
